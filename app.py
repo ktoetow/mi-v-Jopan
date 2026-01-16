371afd2a5d2bdd592b8fa450b09424bde724c735
mi v Jopan <johndoe@example>
2026-01-16 15:53:34 +0700

    чуть чуть написала в app.py и создала html

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-123
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-аылар
+app = FastAPI(
+    title="TutorBook",
+    version="0.1.0"
+)
+
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    return 
+@app.get("/api/health")
+async def health_check():
+    return {"status": "ok", "message": ""}
