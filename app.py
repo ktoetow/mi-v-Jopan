08c8093e52984831b20075ad163c286e20a5bcc2
mi v Jopan <johndoe@example>
2026-01-16 16:27:55 +0700

    css красивый более менее, не много доделан app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
 app = FastAPI(
@@ -6,9 +9,13 @@ app = FastAPI(
     version="0.1.0"
 )
 
+app.mount('/static',StaticFiles(directory='static'), name='static')
+templates=Jinja2Templates(directory='templates')
+
 @app.get("/", response_class=HTMLResponse)
-async def home():
-    return 
+async def home(request: Request):
+    return templates.TemplateResponse("base.html", {"request": request})
 @app.get("/api/health")
 async def health_check():
-    return {"status": "ok", "message": ""}
+    return {"status": "ok", "message": "Все работает"}
+
