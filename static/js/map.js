a9378346532a1dbca9f625ebb99f21222c671dd0
mi v Jopan <johndoe@example>
2026-01-20 12:30:47 +0700

    карта подключена

---
+++ static/js/map.js
@@ -0,0 +1,8 @@
+function initYandexMap() {
+    var myMap = new ymaps.Map('maps', {
+        center: [92.87369240561432, 56.01190102599454], 
+        zoom: 16
+    });
+}
+
+ymaps.ready(initYandexMap);
--- static/map.js
+++
@@ -1,8 +0,0 @@
-function init() {
-    let map = new ymaps.Map('maps', {
-        center: [92.87369240561432, 56.01190102599454],
-        zoom: 16
-    });
-}
-
-ymaps.ready(init);
