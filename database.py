3b72886b519c0646252c1ba2a6d8510eb88d8429
mi v Jopan <johndoe@example>
2026-01-22 15:03:43 +0700

    добавлен database.py

---
+++ database.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import close_all_sessions, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+
+DATABASE_URL = "postgresql+asyncpg://postgres:ktoeto1243@localhost:5432/tutorbook"
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,          
+    poolclass=NullPool  
+)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
